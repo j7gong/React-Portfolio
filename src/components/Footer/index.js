b0d2586171c2fab9fbaa7d7994926f52d500410c
Jingsi <gongjingsi@hotmail.com>
2022-11-11 00:43:23 -0500

    add stylesheet and components including about, nav, portfolio, resume, and footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <a
+        className="my-5"
+        href="https://github.com/j7gong"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+    My Github
+    </a>
+  );
+}
+
+export default Footer;
